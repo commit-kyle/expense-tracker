83f841ca97a7bca5c321b14fd33ede3bbd031be1
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-22 15:04:02 +0200

    Removed date functionalities and provided its own component

--- expense-tracker/src/components/ExpenseItem/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import './ExpenseItem.css';
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 const ExpenseItem = props => {
 
-  return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+		</div>
+	);
+};
 
 export default ExpenseItem;

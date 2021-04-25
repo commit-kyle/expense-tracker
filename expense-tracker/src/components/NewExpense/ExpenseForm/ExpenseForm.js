aff8f2b6179fcc485e3050f0d0eecb69f2609700
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 00:56:44 +0200

    Lifting user input data upward

--- expense-tracker/src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = props => {
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
 	const [enteredDate, setEnteredDate] = useState('');
@@ -19,6 +19,8 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate)
     }
+
+    props.onSaveExpData(expenseData);
 
     setEnteredTitle('');
     setEnteredAmount('');

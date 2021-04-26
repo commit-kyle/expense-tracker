a320d37cf83bf36bad6073f1c3540dfae2f8dede
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 21:02:27 +0200

    Reconfigured conditional rendering of content into its own component and provided conditional return statements

--- expense-tracker/src/components/ExpensesList/ExpensesList.js
+++ expense-tracker/src/components/ExpensesList/ExpensesList.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import './ExpensesList.css';
 
 const ExpensesList = props => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	if (props.items.length === 0) {
+		return (
+			<h2 className="expenses-list__fallback">
+				There are no expenses for this year.
+			</h2>
+		);
+	}
 
-export default ExpensesList
+	return (
+		<ul className={'expenses-list'}>
+			{props.items.map(expense => (
+				<ExpenseItem
+					key={expense.id}
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>
+			))}
+		</ul>
+	);
+};
+
+export default ExpensesList;

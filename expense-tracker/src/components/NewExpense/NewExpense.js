cf15709c656f8b239614aedd4f57701c379db7a3
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 21:33:57 +0200

    Set up and manage state to conditionally manage form component rendering

--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm/ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = props => {
-  const saveExpDataHandler = expData => {
-    const expenseData = {
-      ...expData,
-      id: Math.random().toString(),
-    };
+	const [isEditing, setIsEditing] = useState(false);
 
-    props.onAddExpense(expenseData);
-  }
+	const saveExpDataHandler = expData => {
+		const expenseData = {
+			...expData,
+			id: Math.random().toString(),
+		};
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpData={saveExpDataHandler} />
-    </div>
-  )
-}
+		props.onAddExpense(expenseData);
+		setIsEditing(false);
+	};
 
-export default NewExpense
+	const newExpenseHandler = () => {
+		setIsEditing(true);
+	};
+
+	const cancelExpenseHandler = () => {
+		setIsEditing(false);
+	};
+
+	return (
+		<div className="new-expense">
+			{!isEditing && (
+				<button onClick={newExpenseHandler}>Add New Expense</button>
+			)}
+			{isEditing && (
+				<ExpenseForm
+					onCancel={cancelExpenseHandler}
+					onSaveExpData={saveExpDataHandler}
+				/>
+			)}
+		</div>
+	);
+};
+
+export default NewExpense;

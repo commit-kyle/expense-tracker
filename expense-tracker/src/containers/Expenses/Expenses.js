f3f69f2fa985b8c7776541f238404d311316eb12
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 17:39:11 +0200

    Configured dynamic rendering for expense items

--- expense-tracker/src/containers/Expenses/Expenses.js
+++ expense-tracker/src/containers/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 import './Expenses.css';
 import ExpenseItem from '../../components/ExpenseItem/ExpenseItem';
@@ -10,33 +10,23 @@ const Expenses = props => {
 
 	const filterChangeHandler = selectedYear => {
 		setFilteredYear(selectedYear);
-	}
+	};
 
-  return (
-    <Card className="expenses">
-			<ExpenseFilter selectedYear={filteredYear} onFilterChange={filterChangeHandler} />
-      <ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
+	return (
+		<Card className="expenses">
+			<ExpenseFilter
+				selectedYear={filteredYear}
+				onFilterChange={filterChangeHandler}
 			/>
-			<ExpenseItem
-				title={props.items[1].title}
-				amount={props.items[1].amount}
-				date={props.items[1].date}
-			/>
-			<ExpenseItem
-				title={props.items[2].title}
-				amount={props.items[2].amount}
-				date={props.items[2].date}
-			/>
-			<ExpenseItem
-				title={props.items[3].title}
-				amount={props.items[3].amount}
-				date={props.items[3].date}
-			/>
-    </Card>
-  )
-}
+			{props.items.map(expense => (
+				<ExpenseItem
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>
+			))}
+		</Card>
+	);
+};
 
 export default Expenses;

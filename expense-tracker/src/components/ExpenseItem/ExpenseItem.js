a434b6c5dfabcc9dbddaf30ee2539c5d3e1bb514
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 21:11:15 +0200

    Corrected document structure by implementing semantically correct HTML markup

--- expense-tracker/src/components/ExpenseItem/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem/ExpenseItem.js
@@ -5,15 +5,16 @@ import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../containers/Card/Card';
 
 const ExpenseItem = props => {
-
 	return (
-		<Card className="expense-item">
-			<ExpenseDate date={props.date} />
-			<div className="expense-item__description">
-				<h2>{props.title}</h2>
-				<div className="expense-item__price">R{props.amount}</div>
-			</div>
-		</Card>
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date} />
+				<div className="expense-item__description">
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">R{props.amount}</div>
+				</div>
+			</Card>
+		</li>
 	);
 };
 

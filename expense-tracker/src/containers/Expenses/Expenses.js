307cd7f824ca89a4e139a7d0d68031b607d41b59
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 23:08:57 +0200

    Imported ExpensesChart component to expose it to filteredExpenses data

--- expense-tracker/src/containers/Expenses/Expenses.js
+++ expense-tracker/src/containers/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import ExpensesList from '../../components/ExpensesList/ExpensesList';
 import Card from '../Card/Card';
 import ExpenseFilter from '../../components/ExpenseFilter/ExpenseFilter';
+import ExpensesChart from './ExpensesChart/ExpensesChart';
 
 const Expenses = props => {
 	const [filteredYear, setFilteredYear] = useState('2020');
@@ -22,6 +23,7 @@ const Expenses = props => {
 				selectedYear={filteredYear}
 				onFilterChange={filterChangeHandler}
 			/>
+			<ExpensesChart expenses={filteredExpenses} />
 			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);

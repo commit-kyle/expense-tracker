ba34129bc8bcd7707384dbd364a4329adeeb1c2e
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 18:16:09 +0200

    Added props to handle data lifting to parent component

--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm/ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = props => {
   const saveExpDataHandler = expData => {
     const expenseData = {
       ...expData,
       id: Math.random().toString(),
-    }
+    };
+
+    props.onAddExpense(expenseData);
   }
 
   return (

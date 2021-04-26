da3c5a4d067586d6f8b40acd415cad0a261ac2b1
Kyle Buttress <kylebuttress11@gmail.com>
2021-04-26 17:31:28 +0200

    Retrieved event from select element and passed the selected value down from the parent

--- expense-tracker/src/components/ExpenseFilter/ExpenseFilter.js
+++ expense-tracker/src/components/ExpenseFilter/ExpenseFilter.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './ExpenseFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = props => {
+
+  const filterChangeHandler = e => {
+    props.onChangeFilter(e.target.value);
+  }
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select value={props.selectedYear} onChange={filterChangeHandler} >
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
